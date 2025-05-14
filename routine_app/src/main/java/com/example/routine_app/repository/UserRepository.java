64518a2c38ae532113212393db57cc28725ff6f8
Youbin02 <145626642+Youbin02@users.noreply.github.com>
2025-05-14 19:03:37 +0900

    Delete routine_app directory

--- routine_app/src/main/java/com/example/routine_app/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.routine_app.repository;
-
-import com.example.routine_app.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    User findByUsername(String username);
-}
