aceb1d22f3f2d64d961c9e0c5b0b8948e54b4126
Youbin02 <145626642+Youbin02@users.noreply.github.com>
2025-05-25 14:30:52 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,16 @@
 from multiprocessing import Process
-from ble_receiver import receive_bluetooth_data
-from routine_runner import run_routine_loop
-import logging
-
-logging.basicConfig(level=logging.INFO)
+from rfcomm_server import start_server
+from routine_runner import run_routine_runner
 
 if __name__ == "__main__":
-    try:
-        logging.info("[MAIN] proccess start")
-        p1 = Process(target=receive_bluetooth_data)
-        p2 = Process(target=run_routine_loop)
+    p1 = Process(target=start_server)
+    p2 = Process(target=run_routine_runner)
 
-        p1.start()
-        p2.start()
+    p1.start()
+    p2.start()
 
-        p1.join()
-        p2.join()
+    p1.join()
+    p2.join()
 
     except KeyboardInterrupt:
         logging.info("[MAIN] Manual shutdown requested")
