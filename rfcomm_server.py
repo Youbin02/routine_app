07325bc3f01a80d469ff46c437cb2066a9b3904a
Youbin02 <145626642+Youbin02@users.noreply.github.com>
2025-05-25 16:38:45 +0900

    Update rfcomm_server.py

--- rfcomm_server.py
+++ rfcomm_server.py
@@ -2,12 +2,9 @@ import bluetooth
 import json
 import sqlite3
 import time
-import os
 import logging
-import threading
 
 DB_PATH = "/home/pi/LCD_final/routine_db.db"
-OUTBOX_PATH = "/tmp/routine_outbox.json"
 
 logging.basicConfig(level=logging.INFO)
 
@@ -41,21 +38,6 @@ def save_to_db(data):
     conn.commit()
     conn.close()
 
-def monitor_outbox_and_send(client_sock):
-    logging.info("[📤] routine complete outbox look start")
-    while True:
-        try:
-            if os.path.exists(OUTBOX_PATH):
-                with open(OUTBOX_PATH, "r") as f:
-                    data = f.read().strip()
-                    if data:
-                        client_sock.send((data + '\n').encode())
-                        logging.info(f"[📤] routine complete data send: {data}")
-                os.remove(OUTBOX_PATH)
-        except Exception as e:
-            logging.error(f"[❌] outbox send failed: {e}")
-        time.sleep(2)
-
 def start_rfcomm_server():
     while True:
         try:
@@ -67,9 +49,6 @@ def start_rfcomm_server():
             logging.info("[🔌] RFCOMM Bluetooth connect wait...")
             client_sock, address = server_sock.accept()
             logging.info(f"[✅] connected: {address}")
-
-            # outbox 전송 스레드 시작
-            threading.Thread(target=monitor_outbox_and_send, args=(client_sock,), daemon=True).start()
 
             buffer = ""
             while True:
@@ -84,7 +63,7 @@ def start_rfcomm_server():
                     if not line:
                         continue
 
-                    logging.info(f"[📥] recv date: {line}")
+                    logging.info(f"[📥] send data: {line}")
                     try:
                         json_data = json.loads(line)
                         if isinstance(json_data, list):
@@ -97,7 +76,7 @@ def start_rfcomm_server():
                     except Exception as e:
                         logging.error(f"[❌] JSON error: {e}")
         except Exception as e:
-            logging.error(f"[❌] connect or recv error: {e}")
+            logging.error(f"[❌] connect or receive error: {e}")
             time.sleep(1)
         finally:
             try:
