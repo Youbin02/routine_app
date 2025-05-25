e3762c6b4dd34443b42c6fc1ee131e717ca7e07f
Youbin02 <145626642+Youbin02@users.noreply.github.com>
2025-05-25 16:03:31 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 from multiprocessing import Process
-from rfcomm_server import start_server
-from routine_runner import run_routine_runner
+from rfcomm_server import start_rfcomm_server
+from routine_runner import run_routine_loop
 
 if __name__ == "__main__":
-    p1 = Process(target=start_server)
-    p2 = Process(target=run_routine_runner)
+    try:
+        p1 = Process(target=start_rfcomm_server)
+        p2 = Process(target=run_routine_loop)
 
-    p1.start()
-    p2.start()
+        p1.start()
+        p2.start()
 
-    p1.join()
-    p2.join()
+        p1.join()
+        p2.join()
+
+    except KeyboardInterrupt:
+        print("[🛑] main proccess stopped")
